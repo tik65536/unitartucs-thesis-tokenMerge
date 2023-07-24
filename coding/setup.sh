24eddc149122dffb795d4d8fa2873b858740f1f7
root <tik65536@gmail.com>
2023-07-24 02:25:49 +0000

    Changes to be committed: 	new file:   setup.sh

---
+++ coding/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt-get update
+apt-get install vim -y
+apt-get install screen -y
+apt-get install htop -y
+if [[ $PUBLIC_KEY ]]
+then
+    mkdir -p ~/.ssh
+    chmod 700 ~/.ssh
+    cd ~/.ssh
+    echo $PUBLIC_KEY >> authorized_keys
+    chmod 700 -R ~/.ssh
+    service ssh start
+fi
+cd /workspace/unitartucs-thesis-tokenMerge/coding
+pip3 install -r requirements.txt
+python -m spacy download en_core_web_md
