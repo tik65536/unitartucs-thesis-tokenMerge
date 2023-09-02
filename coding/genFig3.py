02f537773ef584898c2781f802aabbf490160f31
dick <tik65536@gmail.com>
2023-09-02 21:30:01 +0800

    Changes to be committed: 	new file:   genFig3.py 	new file:   list

---
+++ coding/genFig3.py
@@ -0,0 +1,178 @@
+#!/usr/bin/python3
+import pickle
+import os
+import glob
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+from IPython.display import HTML
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
+import spacy
+import pandas as pd
+import pymp
+# For better quality video
+nlp=spacy.load("en_core_web_md")
+
+with open('./first1000_testset.plk','rb') as f:
+    test1=pickle.load(f)
+with open('./test_totalCorpora.plk','rb') as f:
+    totalCount=pickle.load(f)
+with open('./test_negCorpora.plk','rb') as f:
+    negCount=pickle.load(f)
+
+accy=pd.read_csv('./ResultLog/Curl_MergeNOUN_NewR1_val.csv')
+
+for k in totalCount.keys():
+    total=totalCount[k]
+    neg=negCount[k]
+    negCount[k]=neg/total
+
+posidx=np.array(test1[test1['label']==1].index)
+negidx=np.array(test1[test1['label']==0].index)
+samplesize=200
+nsmpidx=np.random.choice(negidx,samplesize,replace=False)
+psmpidx=np.random.choice(posidx,samplesize,replace=False)
+with open('./Master_vocab.pkl','rb') as f:
+    vocab=pickle.load(f)
+tok2id=vocab['tok2id']
+id2tok=vocab['id2tok']
+endid=tok2id['e0s']
+docs=nlp.pipe(test1['text'])
+negtextFreq = []
+poslist=[]
+for doc in docs:
+    countlist=[]
+    plist=[]
+    for token in doc:
+        if(token.is_stop==False and token.is_punct==False):
+            countlist.append(negCount[token.text])
+            if(token.text!='s0s' and token.text!='e0s'):
+                plist.append(token.pos)
+            elif(token.text=='s0s'):
+                plist.append(70)
+            else:
+                plist.append(78)
+    negtextFreq.append(countlist)
+    poslist.append(plist)
+
+for i,r in enumerate(negtextFreq):
+    if len(r)<175:
+        negtextFreq[i]=(r+([negCount['e0s']]*(175-len(r))))
+    else:
+        negtextFreq[i]=negtextFreq[i][:175]
+for i,r in enumerate(poslist):
+    if len(r)<175:
+        poslist[i]=(r+([78]*(175-len(r))))
+    else:
+        poslist[i]=poslist[i][:175]
+
+negtextFreq=np.array(negtextFreq)
+poslist=np.array(poslist)
+#postextFreq=np.array(postextFreq)
+path='/home/dick/RunpodData/Curl_MergeNOUN_NewR1_seqlen25_sldie25_batch1000_opt25_dynamicOptFalse_train15000_ksize2_e3_BiDirectionTrue_HL3_HS3_P1.0_MaxMerge3_MinMerge2_preprocess1.0_20230809-063952'
+curdataList = glob.glob(f'{path}/val_curldata_*.plk')
+curldata={}
+for file in curdataList:
+    with open(f'{file}','rb') as f:
+        try:
+            tmp=pickle.load(f)
+            i=list(tmp.keys())[0]
+            try:
+                curldata[i]=tmp[i]['LSTM']
+            except KeyError:
+                curldata[i]=tmp[i]
+        except EOFError:
+            pass
+predictList = glob.glob(f'{path}/EstPred_*.plk')
+predict=np.zeros((1000,len(predictList)))
+for file in predictList:
+    ep=file.split("_")[-1].split(".")[0]
+    with open(f'{file}','rb') as f:
+        try:
+            tmp=pickle.load(f)
+            predict[:,int(ep)]=tmp.T
+        except EOFError:
+            pass
+
+lastepoch=sorted(list(curldata.keys()))[-1]
+print(f'Last Epoch: {lastepoch}')
+keys=list(curldata[0]['forward'].keys())
+plt.rcParams['grid.color'] = "black"
+with pymp.Parallel(1) as p:
+    for epoch in p.range(10,lastepoch+1):
+        direction='forward'
+        epoch_in=curldata[epoch][direction][0][:,:25,:]
+        epoch_curl=curldata[epoch][direction][0][:,25:50,:]
+
+        for k in range(1,len(keys)):
+            epoch_in = np.hstack((epoch_in,curldata[epoch][direction][keys[k]][:,:25,:]))
+            epoch_curl = np.hstack((epoch_curl,curldata[epoch][direction][keys[k]][:,25:50,:]))
+        in_l=np.linalg.norm(epoch_in,axis=-1)
+        epoch_in_unit = epoch_in/ in_l.reshape(1000,175,1)
+        curl_l=np.linalg.norm(epoch_curl,axis=-1)
+        epoch_curl_unit = epoch_curl/ curl_l.reshape(1000,175,1)
+        relativePosition=np.zeros((1000,175))
+        for i in range(175):
+            rp=np.ceil((i/test1.loc[:]['length'])*100)
+            relativePosition[:,i]=rp
+        curldiff=[]
+        curl_unit_diff=[]
+
+        for i in range(0,174):
+            curldiff.append(epoch_curl[:,i+1,:]-epoch_curl[:,i,:])
+            curl_unit_diff.append(epoch_curl_unit[:,i+1,:]-epoch_curl_unit[:,i,:])
+        curldiff=np.array(curldiff)
+        curldiff=np.moveaxis(curldiff,0,1)
+        curldiff_norm=np.linalg.norm(curldiff,axis=-1)
+        curldiff_unit=curldiff/curldiff_norm.reshape(epoch_curl.shape[0],174,1)
+
+
+        curl_unit_diff=np.array(curl_unit_diff)
+        curl_unit_diff=np.moveaxis(curl_unit_diff,0,1)
+        curl_unit_diff_norm=np.linalg.norm(curl_unit_diff,axis=-1)
+        curl_unit_diff_unit=curl_unit_diff/curl_unit_diff_norm.reshape(epoch_curl.shape[0],174,1)
+
+        prediction=predict[:,epoch]
+        prediction=np.where(prediction>0.95,1,prediction)
+        prediction=np.where(prediction<0.05,0,prediction)
+        relativePosition2=np.where(relativePosition>100,-1,relativePosition)
+        curldiffN=curl_unit_diff_unit
+        negcolormap = cm.bwr
+        co=np.zeros((1000,175))
+        co[np.where(prediction==1)]=1
+        norm = Normalize()
+        norm.autoscale(co)
+        nco=negcolormap(norm(co))
+        r,c=np.where(relativePosition2[:,:174]==-1)
+        nco[r,c,-1]=0
+        r,c=np.where((poslist[:,:174]==70)|(poslist[:,:174]==78))
+        nco[r,c,-1]=0
+        r,c=np.where((curl_unit_diff_norm<0.25))
+        nco[r,c,-1]=0
+
+        start,stop=0,175
+        plt.close('all')
+        divfig=plt.figure(figsize=(10,10))
+        curlax=divfig.add_subplot(projection='3d')
+        for i in range(start,stop):
+            keep=np.where((relativePosition2[:,i]>0) & (relativePosition2[:,i]<100))[0]
+            curlax.quiver(epoch_curl_unit[keep,i:i+1,0], epoch_curl_unit[keep,i:i+1,1], epoch_curl_unit[keep,i:i+1,2],
+                  curldiffN[keep,i:i+1,0],curldiffN[keep,i:i+1,1], curldiffN[keep,i:i+1,2] ,
+                  length=0.05, normalize=True,color=nco[keep,i:i+1],linewidth=0.5)
+            #curlax.scatter3D(curldiffN[keep,i:i+1,0], curldiffN[keep,i:i+1,1],curldiffN[keep,i:i+1,2],s=0.1,c=nco[keep,i:i+1],depthshade=True)
+            #curlax.quiver(sample[keep,i:i+1,0], sample[keep,i:i+1,1], sample[keep,i:i+1,2],
+            #              sample_curl[keep,i:i+1,0] , sample_curl[keep,i:i+1,1], sample_curl[keep,i:i+1,2] ,
+            #              length=0.2,colors=nco[keep,i:i+1],normalize=True)#
+        curlax.set_xlabel('LSTM output dim 1')
+        curlax.set_ylabel('LSTM output dim 2')
+        curlax.set_zlabel('LSTM output dim 3')
+        #curlax.view_init(elev=60, azim=-90)
+        curlax.set_facecolor('gray')
+        #sm = plt.cm.ScalarMappable(cmap=cm.bwr, norm=norm)
+        #cbar=divfig.colorbar(sm,ax=curlax,shrink=0.5,pad=0.2)
+        #cbar.set_label('Negative Tokens Relative Frequence')
+        #curlax.set_title(f"BaseCase LSTM output unit norm Epoch {epoch} Accy:{accy.loc[epoch]['Accy']} \n ValidationSet (1000 Samples) ColorMap=Negative Sentiment Token Frequence")
+        plt.tight_layout()
+        plt.savefig(f'./GenFig/epoch_{epoch}')
+        print(f'Epoch {epoch} Done')
